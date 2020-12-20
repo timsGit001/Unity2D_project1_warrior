902bd4cd125157e72b9b72f2fa417bc99f40df36
timsGit001 <75553502+timsGit001@users.noreply.github.com>
2020-12-20 17:02:07 +0800

    試加腳本

--- 2D_Warrior/Assets/Scripts/Car.cs
+++ 2D_Warrior/Assets/Scripts/Car.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /* 基本設定 */
+    [Header("基本設定")]
+    [Tooltip("汽車高度(公尺)"), Min(1)]
+    public int height = 1;                                   // 高度
+    [Tooltip("汽車重量(噸)"), Range(0.01f, 5.00f)]
+    public float weight = 0.01f;                             // 重量
+    [Tooltip("汽車品牌")]
+    public string brand = "請填品牌";                         // 品牌
+    [Tooltip("是否有天窗設計")]
+    public bool hasTopWindow = false;                        // 是否 有天窗
+    [Tooltip("車體顏色")]
+    public Color color = new Color(0.0f, 0.0f, 0.0f, 1.0f);  // 顏色
+    [Tooltip("汽車位置")]
+    public Vector4 location = new Vector4(1.1f,3.1f,0f,9.0f); // 座標
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Tooltip("駕駛")]
+    public GameObject driver;
+    [Tooltip("加速器")]
+    public ParticleSystem n2;
+    [Tooltip("監視器")]
+    public Camera cam;
+
+
+    /* 機密設定 */
+    [Header("機密設定")]
+    [Tooltip("馬力"), Range(300, 5000), SerializeField]
+    private int power = 300;                             // 馬力
+    [Tooltip("駕駛位置")]
+    private Vector2Int driverLocation = Vector2Int.zero; // 駕駛座標
+    [Tooltip("造型噴漆"), SerializeField]
+    private Sprite painting;                             // 造型噴漆
+    [Tooltip("特色樂"), SerializeField]
+    private AudioClip bgm;                               // 特色樂
 }
---
+++ 2D_Warrior/Assets/Scripts/Warrior.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Warrior : MonoBehaviour
+{
+    /* 公開 設定 */
+    // 角色相關
+    [Header("移動速度"), Range(0,1000)]
+    public float moveSpeed  = 10.5f; // 移動速度
+    [Header("跳躍高度"), Range(0, 3000)]
+    public int jumpHeight  = 100;    // 跳躍高度
+    [Header("是否在地板上"), Tooltip("是否在地板上?(是:T 否:F)")]
+    public bool isOnFloor  = false;  // 是否在地板上
+    [Range(0, 200)]
+    public int hp = 100;             // 血量
+
+    // 武器相關
+    [Header("子彈"), Tooltip("請提供子彈物件")]
+    public GameObject bullet;        // 子彈物件
+    [Header("子彈生成點"), Tooltip("請提供座標")]
+    public Transform bulletBirthLoc; // 子彈生成點
+    [Range(0, 5000)]
+    public int bulletSpeed = 800;    // 子彈速度
+    [Header("開槍音效"), Tooltip("請提供開槍音效")]
+    public AudioClip shootAudio;     // 開槍音效
+
+    /* 私人 設定 */
+    private AudioSource m_audioSource; // 音效來源
+    private Rigidbody2D m_rigidbody2D; // 2D 剛體
+    private Animator m_animator;       // 動畫控制器
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
