ac1fcd50d239665d9d0ad81ec7c7bb30a78ab433
timsGit001 <75553502+timsGit001@users.noreply.github.com>
2021-01-03 13:00:54 +0800

    場景控制

---
+++ 2D_Warrior/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene("MainGame01");
+    }
+
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+}
