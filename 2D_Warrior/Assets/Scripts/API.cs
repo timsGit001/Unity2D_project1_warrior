7c1b5f2fc4c46179ccb3f1722058c253edb1ead1
timsGit001 <75553502+timsGit001@users.noreply.github.com>
2021-01-03 12:00:56 +0800

    練習API

---
+++ 2D_Warrior/Assets/Scripts/API.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class API : MonoBehaviour
+{
+    public Transform traA;
+
+    public Transform traB;
+    public SpriteRenderer srB;
+    public GameObject objB;
+    float x = 0.1f;
+
+    public Camera cam;
+
+    public void Start()
+    {
+        print("Cordinate:" + traA.position);
+        traB.position = new Vector3(0, 1, 0);
+
+        cam.backgroundColor = new Color(0.0f, 0.0f, 0.0f);
+
+        print("圖片的顏色:" + srB.color);
+        print("物件的圖層:" + objB.layer);
+        srB.flipX = false;
+        objB.layer = 5;
+    }
+
+    public void Update()
+    {
+        CtrlTraB();
+
+    }
+
+    void CtrlTraB() {
+        // traB.Rotate(new Vector3(0, 0, 30));
+        if (traB.position.x > 5f)
+        {
+            srB.flipX = true;
+            x = -0.5f;
+        }
+        else if (traB.position.x < -5f)
+        {
+            srB.flipX = false;
+            x = 0.5f;
+        }
+
+        traB.Translate(new Vector3(x, 0, 0), Space.World);
+    }
+}
---
+++ 2D_Warrior/Assets/Scripts/APIStatic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class APIStatic : MonoBehaviour
+{
+    [Header("移動速度"), Range(0f,100f)]
+    public float Speed = 10f;
+    [Header("跳躍高度"), Range(0,2000)]
+    public int Jump = 20;
+    [Header("對話內容")]
+    public string Talk = "咕咕咕~";
+    [Header("是否取得雞蛋")]
+    public bool Egg = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("遊戲開始");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Talk);
+    }
+}
